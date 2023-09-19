8043f02007e0ab7d7098220c482eb7051996120d
richardmozenao <rmunozh@unal.edu.co>
2023-09-19 16:43:10 -0500

    atributos y metodos clase recibo

--- src/recibo.java
+++ src/recibo.java
@@ -1,3 +1,19 @@
 public class recibo {
-    
+    int idCliente;
+    int idRecibo;
+    float precioTotal;
+    float precioFinal;
+    float descuento;
+
+    public float getPrecioTotal() {
+        return precioTotal;
+    }
+
+    public float getPrecioFinal() {
+        return precioFinal;
+    }
+
+    public float getDescuento() {
+        return descuento;
+    }
 }
