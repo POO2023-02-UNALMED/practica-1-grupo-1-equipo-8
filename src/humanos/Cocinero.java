6ada62f98f5bf375211342ef8aefc7435a1216d5
Nicolas Gomez Echeverri <141961453+NicolasGomezEcheverri@users.noreply.github.com>
2023-09-20 17:45:17 -0500

    expancion a Persona, Cliente y Cocinero
    se agregaron algunos metodos y atributos

--- src/humanos/Cocinero.java
+++ src/humanos/Cocinero.java
@@ -1,5 +1,16 @@
 package humanos;
 
 public class Cocinero extends Persona{
-    
+    private int experiencia;
+    private int salario;
+    private int papaya;
+
+    public Cocinero() {}
+
+    public Cocinero(String nombre, String apellido, int dinero, int experiencia, int salario, int papaya) {
+        super(nombre, apellido, dinero);
+        this.experiencia = experiencia;
+        this.salario = salario;
+        this.papaya = papaya;
+    }
 }
--- src/humanos/Persona.java
+++ src/humanos/Persona.java
@@ -1,5 +1,37 @@
 package humanos;
 
 public class Persona {
+    private String nombre;
+    private String apellido;
+    private int dinero;
+
+    public Persona() {}
+
+    public Persona(String nombre, String apellido, int dinero) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dinero = dinero;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApelllido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getDinero() {
+        return dinero;
+    }
+
     
 }
