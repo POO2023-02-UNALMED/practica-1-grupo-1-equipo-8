c0e24beab7efd14d10104215484869eb91d28205
Samuel Castaño Mira <scastanomi@unal.edu.co>
2023-09-23 10:56:24 -0500

    Creación clase Ingrediente
    creé una lista en la clase Prodcuto para crear un buen constructor para Ingrediente

--- src/comida/Ingrediente.java
+++ src/comida/Ingrediente.java
@@ -1,3 +1,34 @@
+package comida;
+
 public class Ingrediente {
+    String nombre;
+    int cantidad;
+    double precio;
+    //String direccion; ?
     
+    public Ingrediente() {
+    	Producto.Ingredientes.add(this);
+    }
+    
+    public Ingrediente(String nombre1,int cantidad1,double precio1) {
+    	this.nombre =nombre1;
+    	this.cantidad=cantidad1;
+    	this.precio=precio1;
+    	Producto.Ingredientes.add(this);
+    }
+    
+  //getters y setters de los atributos
+    
+    public String getNombre() {return nombre;}
+    public void setNombre(String Newnombre) {this.nombre =Newnombre;}
+	public int getCantidad() {return cantidad;}
+	public void setCosto(int Newcantidad) {this.cantidad = Newcantidad;}
+	public double getPrecio() {return precio;}
+	public void setPrecio(double Newprecio) {this.precio = Newprecio;}
+	
+	//metodo para crear
+	
+	public static Ingrediente crearIngrediente(String Nnombre,int Ncantidad, double Nprecio) {
+		return new Ingrediente(Nnombre,Ncantidad,Nprecio);
+	}
 }
--- src/comida/Producto.java
+++ src/comida/Producto.java
@@ -1,3 +1,9 @@
+package comida;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Producto {
+	public static List<Ingrediente> Ingredientes = new ArrayList<Ingrediente>();
     
 }
