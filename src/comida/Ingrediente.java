73e96f3545c35876c3d6831afc0030913d94bb84
Samuel Castaño Mira <scastanomi@unal.edu.co>
2023-09-24 23:17:26 -0500

    Creación constructores y metodos sobrecargaos para la creación de ingrediente
    Un constuctor que recibe nombre y precio y asigna los valores, otro constructor sobrecargado que recibe solo nombre y al precio le da un valor aleatorio entre 300 y 3000, y metodos sobrecargados que llaman a los constructores

--- src/comida/Ingrediente.java
+++ src/comida/Ingrediente.java
@@ -1,34 +1,39 @@
 package comida;
 
+import java.util.Random;
+
 public class Ingrediente {
-    String nombre;
-    int cantidad;
-    double precio;
-    //String direccion; ?
+    private String nombre;
+    // int cantidad;
+    private float precio;
     
-    public Ingrediente() {
-    	Producto.Ingredientes.add(this);
-    }
-    
-    public Ingrediente(String nombre1,int cantidad1,double precio1) {
+  //constructores sobrecargados  
+  
+    public Ingrediente(String nombre1,float precio1) {
     	this.nombre =nombre1;
-    	this.cantidad=cantidad1;
     	this.precio=precio1;
-    	Producto.Ingredientes.add(this);
     }
-    
+    public Ingrediente(String nombre1) {
+    	this.nombre =nombre1;
+    	Random aleatorio = new Random();
+    	int numeroAleatorio = aleatorio.nextInt(2701)+300; // Genera un entero entre 0 y 100 (ambos inclusive).
+    	this.precio=numeroAleatorio; 
+    }  
   //getters y setters de los atributos
     
     public String getNombre() {return nombre;}
     public void setNombre(String Newnombre) {this.nombre =Newnombre;}
-	public int getCantidad() {return cantidad;}
-	public void setCosto(int Newcantidad) {this.cantidad = Newcantidad;}
+	//public int getCantidad() {return cantidad;}
+	//public void setCantidad(int Newcantidad) {this.cantidad = Newcantidad;}
 	public double getPrecio() {return precio;}
-	public void setPrecio(double Newprecio) {this.precio = Newprecio;}
+	public void setPrecio(float Newprecio) {this.precio = Newprecio;}
 	
-	//metodo para crear
+	//metodos para crear
 	
-	public static Ingrediente crearIngrediente(String Nnombre,int Ncantidad, double Nprecio) {
-		return new Ingrediente(Nnombre,Ncantidad,Nprecio);
+	public static Ingrediente crearIngrediente(String Nnombre, float Nprecio) {
+		return new Ingrediente(Nnombre,Nprecio);
+	}
+	public static Ingrediente crearIngrediente(String Nnombre) {
+		return new Ingrediente(Nnombre);
 	}
 }
